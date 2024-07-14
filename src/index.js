303ee96f43188b8df77700f003cf9ddca0e98ceb
Arpit-23 <arpitmali23053@gmail.com>
2024-07-15 00:44:50 +0530

    hello msg

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //apply change for git
+//hello
 root.render(
  <BrowserRouter>
    <AppContextProvider>
